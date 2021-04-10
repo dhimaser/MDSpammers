534701eb23e3abd52008f5aa614f1eb05f33da5a
dhimaser <76945596+dhimaser@users.noreply.github.com>
2021-04-11 02:26:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -115,7 +115,7 @@ def klikwa():
  if json.loads(tes.text)["message"] == 'OTP Sent':
   print "     %s[%s%s%s] [%s Sent %s] %sSuccess, FRee FIre %s from %s%s %ssended %s>> %sSUBSCRIBE%sACINAKONGAME"%(pu,ku,"10",pu,hi,pu,pu,"wa",ku,"klikwa",hi,qu,pu,ku)
  else:
-  print "     %s[%s%s%s] [%s Failed %s] %sFailed, FRee FIre %s from %s%s %snot sended %s>> %sMau yang unlimited? %sBikin Sendiri"%(pu,ku,"10",pu,me,pu,pu,"wa",ku,"klikwa",me,qu,pu,ku)
+  print "     %s[%s%s%s] [%s Failed %s] %sFailed, FRee FIre %s from %s%s %snot sended %s>> %sSUBSCRIBE DULU %sACINAKONGAME"%(pu,ku,"10",pu,me,pu,pu,"wa",ku,"klikwa",me,qu,pu,ku)
 def bakmikeraton():
 # huh = requests.post("https://www.bakmikeraton.com/services/identity/requestOTP",headers={'user-agent':'Mozilla/5.0 (Linux; Android 9; vivo 1902) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.136 Mobile Safari/537.36'},data={'destination':'0'+nom,'otpLength':'6'})
  #if json.loads(huh.text)["IsSuccess"] == True:
